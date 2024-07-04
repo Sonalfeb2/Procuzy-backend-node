6c9e771b3ce37570939ff490a097850c474d1998
Sonal Agrawal <sonalfeb7@gmail.com>
2024-07-04 17:16:52 +0530

    Made Headless True

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post("/scrape", async (req, res) => {
   }
 
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(
       `https://medium.com/search?q=${encodeURIComponent(topic)}`,
